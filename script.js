105254e16f35f864862d0baf4fba35369f8f5201
Apon-TBP <146857059+Apon-TBP@users.noreply.github.com>
2023-10-10 20:29:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 function click_submit() {
-  let res = document.getElementById('pass_in')
-  if (res.value.length > 0 ) {
-    window.alert('正解です！') ;
-   window.open("https://tobirapolis.notion.site/TOBIRAPOLIS-9bc44d2cd75d4d4db1a31cc3779ecee9") ;
-  } else {
-    window.alert('パスワードが未入力です') ;
-  }
+	let res = document.getElementById('pass_in')
+	if (res.value.length > 0 ) {
+		window.alert('正解です！') ;	
+		window.open("https://tobirapolis.notion.site/TOBIRAPOLIS-9bc44d2cd75d4d4db1a31cc3779ecee9") ;
+	} else {
+		window.alert('パスワードが未入力です') ;
+	}
 }
 
 function click_links(kind) {
-  switch case (kind) {
-    case 1:
-      window.open("https://www.tobiratory.com/") ;
-      break ;
-    case 2:
-      window.open("https://tobirapolis.notion.site/TOBIRAPOLIS-9bc44d2cd75d4d4db1a31cc3779ecee9") ;
-      break ;
-    case 3:
-      window.open("https://x.com/Tobiratory?s=20") ;
-      break ;
-    default:
-  }
-
+	switch (kind) {
+	case 1:
+		window.open("https://www.tobiratory.com/") ;
+		break ;
+	case 2:
+		window.open("https://tobirapolis.notion.site/TOBIRAPOLIS-9bc44d2cd75d4d4db1a31cc3779ecee9") ;
+		break ;
+	case 3:
+		window.open("https://x.com/Tobiratory?s=20") ;
+		break ;
+	default:
+	break ;
+  	}
+}
