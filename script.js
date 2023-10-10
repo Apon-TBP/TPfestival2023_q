b261374087153343b718c9074f6324e0d6bc7f41
Apon-TBP <146857059+Apon-TBP@users.noreply.github.com>
2023-10-11 00:17:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function click_submit() {
-	const D_PASS : string = "おんがく" ;
-	let res = document.getElementById('pass_in')
+	const D_PASS : string = "おんがく" ;		//正解を定義
+	let res = document.getElementById('pass_in')	//記入したパスワードを取得
+	//パスワードの文字数は0より大きいか？
 	if (res.value.length > 0 ) {
-		if (res == D_PASS ) {
+		//パスワードの文字列は正解と一致しているか？
+		if (res.value == D_PASS ) {
 			window.alert('正解です！') ;	
 			window.open("https://tobirapolis.notion.site/TOBIRAPOLIS-9bc44d2cd75d4d4db1a31cc3779ecee9") ;
+							//報酬ページを開く
 		} else {
 			window.alert('回答が違うようです…再チャレンジ！') ;
 		}
@@ -14,14 +17,15 @@ function click_submit() {
 }
 
 function click_links(kind) {
+	//リンク種別は？
 	switch (kind) {
-	case 1:
+	case 1:						//トビラトリーHP
 		window.open("https://www.tobiratory.com/") ;
 		break ;
-	case 2:
+	case 2:						//TP祭の公開notion
 		window.open("https://tobirapolis.notion.site/TOBIRAPOLIS-9bc44d2cd75d4d4db1a31cc3779ecee9") ;
 		break ;
-	case 3:
+	case 3:						//トビラトリーのTwitter
 		window.open("https://x.com/Tobiratory?s=20") ;
 		break ;
 	default:
