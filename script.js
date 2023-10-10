42e626cef29d848c2370fa34131639cbd518427a
Apon-TBP <146857059+Apon-TBP@users.noreply.github.com>
2023-10-10 23:42:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function click_submit() {
 	let res = document.getElementById('pass_in')
 	if (res.value.length > 0 ) {
-		window.alert('正解です！') ;	
-		window.open("https://tobirapolis.notion.site/TOBIRAPOLIS-9bc44d2cd75d4d4db1a31cc3779ecee9") ;
+		if (res === D_PASS ) {
+			window.alert('正解です！') ;	
+			window.open("https://tobirapolis.notion.site/TOBIRAPOLIS-9bc44d2cd75d4d4db1a31cc3779ecee9") ;
+		} else {
+			window.alert('回答が違うようです…再チャレンジ！') ;
+		}
 	} else {
 		window.alert('パスワードが未入力です') ;
 	}
