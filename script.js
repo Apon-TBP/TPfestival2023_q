bc4647561ab3754d142e18c54b45fbac52f6e69d
Apon-TBP <146857059+Apon-TBP@users.noreply.github.com>
2023-11-02 20:10:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function sel_chg() {
 	let selstr = document.getElementById('ipt-list').value
+	document.getElementById('xw').style.border ="1px solid black" ;
 	switch(selstr) {
 	case "v1":
 		set_border('v1-1_h1-1') ;
@@ -9,48 +10,48 @@ function sel_chg() {
 		set_border('v1-5_h3-2') ;
 		break ;
 	case "v2":
-		document.getElementById('v2-1').innerText=ipt.charAt(0) ;
-		document.getElementById('v2-2_h1-5').innerText=ipt.charAt(1) ;
-		document.getElementById('v2-3').innerText=ipt.charAt(2) ;
-		document.getElementById('v2-4').innerText=ipt.charAt(3) ;
+		set_border('v2-1') ;
+		set_border('v2-2_h1-5') ;
+		set_border('v2-3') ;
+		set_border('v2-4') ;
 		break ;
 	case "v4":
-		document.getElementById('v4-1_h3-3').innerText=ipt.charAt(0) ;
-		document.getElementById('v4-2_h6-1').innerText=ipt.charAt(1) ;
-		document.getElementById('v4-3').innerText=ipt.charAt(2) ;
-		document.getElementById('v4-4_h7-3').innerText=ipt.charAt(3) ;
-		document.getElementById('v4-5').innerText=ipt.charAt(4) ;
+		set_border('v4-1_h3-3') ;
+		set_border('v4-2_h6-1') ;
+		set_border('v4-3') ;
+		set_border('v4-4_h7-3') ;
+		set_border('v4-5') ;
 		break ;
 	case "v5":
-		document.getElementById('v5-1').innerText=ipt.charAt(0) ;
-		document.getElementById('v5-2').innerText=ipt.charAt(1) ;
-		document.getElementById('v5-3_h6-5').innerText=ipt.charAt(2) ;
-		document.getElementById('v5-4').innerText=ipt.charAt(3) ;
+		set_border('v5-1') ;
+		set_border('v5-2') ;
+		set_border('v5-3_h6-5') ;
+		set_border('v5-4') ;
 		break ;
 	case "h1":
-		document.getElementById('v1-1_h1-1').innerText=ipt.charAt(0) ;
-		document.getElementById('h1-2').innerText=ipt.charAt(1) ;
-		document.getElementById('h1-3').innerText=ipt.charAt(2) ;
-		document.getElementById('h1-4').innerText=ipt.charAt(3) ;
-		document.getElementById('v2-2_h1-5').innerText=ipt.charAt(4) ;
+		set_border('v1-1_h1-1') ;
+		set_border('h1-2') ;
+		set_border('h1-3') ;
+		set_border('h1-4') ;
+		set_border('v2-2_h1-5') ;
 		break ;
 	case "h3":
-		document.getElementById('h3-1').innerText=ipt.charAt(0) ;
-		document.getElementById('v1-5_h3-2').innerText=ipt.charAt(1) ;
-		document.getElementById('v4-1_h3-3').innerText=ipt.charAt(2) ;
+		set_border('h3-1') ;
+		set_border('v1-5_h3-2') ;
+		set_border('v4-1_h3-3') ;
 		break ;
 	case "h6":
-		document.getElementById('v4-2_h6-1').innerText=ipt.charAt(0) ;
-		document.getElementById('h6-2').innerText=ipt.charAt(1) ;
-		document.getElementById('h6-3').innerText=ipt.charAt(2) ;
-		document.getElementById('h6-4').innerText=ipt.charAt(3) ;
-		document.getElementById('v5-3_h6-5').innerText=ipt.charAt(4) ;
+		set_border('v4-2_h6-1') ;
+		set_border('h6-2') ;
+		set_border('h6-3') ;
+		set_border('h6-4') ;
+		set_border('v5-3_h6-5') ;
 		break ;
 	case "h7":
-		document.getElementById('h7-1').innerText=ipt.charAt(0) ;
-		document.getElementById('h7-2').innerText=ipt.charAt(1) ;
-		document.getElementById('v4-4_h7-3').innerText=ipt.charAt(2) ;
-		document.getElementById('h7-4').innerText=ipt.charAt(3) ;
+		set_border('h7-1') ;
+		set_border('h7-2') ;
+		set_border('v4-4_h7-3') ;
+		set_border('h7-4') ;
 	}
 }
 function set_border(obj_id){
