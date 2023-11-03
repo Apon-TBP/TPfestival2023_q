4d658fdaa21cb176286146d940452af210f3c083
Apon-TBP <146857059+Apon-TBP@users.noreply.github.com>
2023-11-03 10:24:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,58 +6,72 @@ function sel_chg() {
 	}
 	//ボーダーを設定
 	let selstr = document.getElementById('ipt-list').value
+	let len = 0 ;
 	switch(selstr) {
-	case "v1":
-		set_border('v1-1') ;
-		set_border('v1-2') ;
-		set_border('v1-3') ;
-		set_border('v1-4') ;
-		set_border('v1-5') ;
-		break ;
-	case "v2":
-		set_border('v2-1') ;
-		set_border('v2-2') ;
-		set_border('v2-3') ;
-		set_border('v2-4') ;
-		break ;
-	case "v4":
-		set_border('v4-1') ;
-		set_border('v4-2') ;
-		set_border('v4-3') ;
-		set_border('v4-4') ;
-		set_border('v4-5') ;
-		break ;
-	case "v5":
-		set_border('v5-1') ;
-		set_border('v5-2') ;
-		set_border('v5-3') ;
-		set_border('v5-4') ;
-		break ;
-	case "h1":
-		set_border('h1-1') ;
-		set_border('h1-2') ;
-		set_border('h1-3') ;
-		set_border('h1-4') ;
-		set_border('h1-5') ;
-		break ;
-	case "h3":
-		set_border('h3-1') ;
-		set_border('h3-2') ;
-		set_border('h3-3') ;
-		break ;
-	case "h6":
-		set_border('h6-1') ;
-		set_border('h6-2') ;
-		set_border('h6-3') ;
-		set_border('h6-4') ;
-		set_border('h6-5') ;
-		break ;
-	case "h7":
-		set_border('h7-1') ;
-		set_border('h7-2') ;
-		set_border('h7-3') ;
-		set_border('h7-4') ;
+	switch (res) {
+	case "v1": len = 5 ;	break ;
+	case "v2": len = 4 ;	break ;
+	case "v4": len = 5 ;	break ;
+	case "v5": len = 4 ;	break ;
+	case "h1": len = 5 ;	break ;
+	case "h3": len = 3 ;	break ;
+	case "h6": len = 5 ;	break ;
+	case "h7": len = 4 ;	break ;
+	}		
+	for (i = 0; i < len; i++ ){
+		set_border(ret + '-' + (i+1)) ;
 	}
+			
+//		set_border('v1-1') ;
+//		set_border('v1-2') ;
+//		set_border('v1-3') ;
+//		set_border('v1-4') ;
+//		set_border('v1-5') ;
+//		break ;
+//	case "v2":
+//		set_border('v2-1') ;
+//		set_border('v2-2') ;
+//		set_border('v2-3') ;
+//		set_border('v2-4') ;
+//		break ;
+//	case "v4":
+//		set_border('v4-1') ;
+//		set_border('v4-2') ;
+//		set_border('v4-3') ;
+//		set_border('v4-4') ;
+//		set_border('v4-5') ;
+//		break ;
+//	case "v5":
+//		set_border('v5-1') ;
+//		set_border('v5-2') ;
+//		set_border('v5-3') ;
+//		set_border('v5-4') ;
+//		break ;
+//	case "h1":
+//		set_border('h1-1') ;
+//		set_border('h1-2') ;
+//		set_border('h1-3') ;
+//		set_border('h1-4') ;
+//		set_border('h1-5') ;
+//		break ;
+//	case "h3":
+//		set_border('h3-1') ;
+//		set_border('h3-2') ;
+//		set_border('h3-3') ;
+//		break ;
+//	case "h6":
+//		set_border('h6-1') ;
+//		set_border('h6-2') ;
+//		set_border('h6-3') ;
+//		set_border('h6-4') ;
+//		set_border('h6-5') ;
+//		break ;
+//	case "h7":
+//		set_border('h7-1') ;
+//		set_border('h7-2') ;
+//		set_border('h7-3') ;
+//		set_border('h7-4') ;
+//	}
 }
 //borderの設定
 function set_border(obj_id){
